ca551d51ca6d854eff9601c9fe2afed7c4602ae9
junesponge <junes.yu@qq.com>
2017-12-22 00:40:38 +0800

    Create SpringBootDemo project.

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,14 +1,36 @@
 package com.example.demo.controller;
 
+import com.example.demo.bean.TblUser;
+import com.example.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class HelloController {
 
-    @RequestMapping(value = {"/hello"},method = RequestMethod.GET)
+    @Autowired
+    private UserRepository userRepository;
+
+    @RequestMapping(value = {"/hello"}, method = RequestMethod.GET)
     public String say(){
         return "world.";
+    }
+
+    @RequestMapping(value = {"/findAll"}, method = RequestMethod.GET)
+    public List<TblUser> findAll() {
+        return userRepository.findAll();
+    }
+
+    @RequestMapping(value = "/addUser", method = RequestMethod.GET)
+    public TblUser addUser(@RequestParam("name") String name, @RequestParam("age") int age) {
+        TblUser user = new TblUser();
+        user.setName(name);
+        user.setAge(age);
+        return userRepository.save(user);
     }
 }
